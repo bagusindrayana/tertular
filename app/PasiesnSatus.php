5af8360c0b29736b735511da329ae4b7b1df2820
bagusindrayana <bagusindrayana@users.noreply.github.com>
2020-06-07 16:45:21 +0800

    update model status pasien

--- app/PasiesnSatus.php
+++ app/PasiesnSatus.php
@@ -3,8 +3,10 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PasiesnSatus extends Model
 {
-    //
+    use SoftDeletes;
+    protected $guarded = [];
 }
